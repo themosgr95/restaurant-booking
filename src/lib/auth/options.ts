35610d5176e13e105a9bd0ca88081907397720fb
themosgr95 <themosph@gmail.com>
2026-01-29 21:31:29 +0100

    Update options.ts

--- src/lib/auth/options.ts
+++ src/lib/auth/options.ts
@@ -1,13 +1,10 @@
 import type { NextAuthOptions } from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
-import { prisma } from "@/lib/db/prisma";
+import { prisma } from "../db/prisma";
 
 export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
   session: { strategy: "database" },
-  // IMPORTANT:
-  // We are NOT enabling providers yet.
-  // We'll add CredentialsProvider in the next step (staff-only login).
   providers: [],
   secret: process.env.NEXTAUTH_SECRET,
 };
